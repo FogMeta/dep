a64b974eb5578873cf58771d8ff7752df8f789f0
will2z <will@fogmeta.com>
2023-09-27 18:09:34 +0800

    optimize: shell tips

--- akash/deploy.sh
+++ akash/deploy.sh
@@ -4,15 +4,21 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 # set environment variables
+ENV_SHELL=${0/deploy.sh/env.sh} 
 echo "init env"
-source env.sh $1
+source $ENV_SHELL $1
 SDLFILE=$2
+IMAGE=$(cat $SDLFILE| grep "image:" | yq -r '.image')
+echo "Deployment: $IMAGE"
+
 # generate & publish cert
-echo "generate & publish cert"
+echo "Generating & publishing cert"
 provider-services tx cert generate client --from $AKASH_KEY_NAME --overwrite
-provider-services tx cert publish client --from $AKASH_KEY_NAME -y
+provider-services tx cert publish client --from $AKASH_KEY_NAME -y >/dev/null 2>&1
+echo "success"
+
 # create deployment
-echo "start creating deployment"
+echo "Start creating deployment: $IMAGE"
 CREATE_LOG=create_deployment.log
 provider-services tx deployment create $SDLFILE --from $AKASH_KEY_NAME -y > $CREATE_LOG
 export AKASH_DSEQ="$(cat $CREATE_LOG | jq -r '.logs[].events[].attributes[] | select(.key == "dseq") | .value' | sed -n '1p')"
@@ -22,37 +28,53 @@ if [ -z "$AKASH_DSEQ" ]; then
 fi
 export AKASH_OSEQ="$(cat $CREATE_LOG | jq -r '.logs[].events[].attributes[] | select(.key == "oseq") | .value' | sed -n '1p')"
 export AKASH_GSEQ="$(cat $CREATE_LOG | jq -r '.logs[].events[].attributes[] | select(.key == "gseq") | .value' | sed -n '1p')"
-echo "dseq: $AKASH_DSEQ, oseq: $AKASH_OSEQ, gseq: $AKASH_GSEQ"
-echo "waitting for provider bids..."
+echo "    dseq: $AKASH_DSEQ"
+echo "    oseq: $AKASH_OSEQ"
+echo "    gseq: $AKASH_GSEQ"
+
+echo "Waitting for providers bids..."
 SLEEP_TIME=15
 if [ $# -eq 3 ]; then
    SLEEP_TIME=$3
 fi
 sleep $SLEEP_TIME
+
 # query provider bids
-echo "query provider bids"
+echo "Querying provider bids..."
 BIDS_LOG=bids.log
 provider-services query market bid list --owner=$AKASH_ACCOUNT_ADDRESS --node $AKASH_NODE --dseq $AKASH_DSEQ --state=open > $BIDS_LOG
+echo "Choosing the best provider..."
 BIDS_SORTED_LOG=bids-sorted.log
 cat $BIDS_LOG | yq '.bids| sort_by(.bid.escrow_account.balance.amount) | reverse | sort_by(.bid.price.amount)' > $BIDS_SORTED_LOG
 export AKASH_PROVIDER="$(cat $BIDS_SORTED_LOG | yq '.[0].escrow_account.owner')"
 PRICE="$(cat $BIDS_SORTED_LOG | yq '.[0].bid.price.amount')"
 AMOUNT="$(cat $BIDS_SORTED_LOG | yq '.[0].escrow_account.balance.amount')"
-echo "selected provider: $AKASH_PROVIDER, bid price: $PRICE, balance amount: $AMOUNT"
+echo "The best provider is:"
+echo "    Address: $AKASH_PROVIDER"
+echo "    Bid Price: $PRICE"
+echo "    Available Balance: $AMOUNT"
 if [ -z "$AKASH_PROVIDER" ]; then 
     echo "not found valid provider"
     exit 1
 fi
+
 # create lease
-echo "start creating lease"
+echo "Start creating the lease"
 LEASE_LOG=lease.log
 provider-services tx market lease create --dseq $AKASH_DSEQ --provider $AKASH_PROVIDER --from $AKASH_KEY_NAME -y > $LEASE_LOG
-provider-services send-manifest $SDLFILE --dseq $AKASH_DSEQ --provider $AKASH_PROVIDER --from $AKASH_KEY_NAME
+provider-services send-manifest $SDLFILE --dseq $AKASH_DSEQ --provider $AKASH_PROVIDER --from $AKASH_KEY_NAME >> $LEASE_LOG
+echo "Start creating the lease"
+echo "    provider: $AKASH_PROVIDER"
+echo "    status:       PASS"
+
 # query deployment url
 echo "waitting for lease url..."
 sleep $SLEEP_TIME
-echo "query lease url"
+echo "Querying the lease url..."
 LEASE_STATUS_LOG=lease-status.log
 provider-services lease-status --dseq $AKASH_DSEQ --from $AKASH_KEY_NAME --provider $AKASH_PROVIDER > $LEASE_STATUS_LOG
 LEASE_URL="$(cat $LEASE_STATUS_LOG | jq -r '.services.web.uris[]| select(. != null)')"
-echo "deploy finished, provider: $AKASH_PROVIDER, price: $PRICE, deployment uris: $LEASE_URL"
+echo "Deploy is finished!"
+echo "    Provider: $AKASH_PROVIDER"
+echo "    Price: $PRICE"
+echo "    URIs: $LEASE_URL"
--- akash/env.sh
+++ akash/env.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 export AKASH_KEY_NAME=$1
-echo "account: $AKASH_KEY_NAME"
+echo "Your Account Name: $AKASH_KEY_NAME"
 export AKASH_KEYRING_BACKEND=os
 export AKASH_ACCOUNT_ADDRESS="$(provider-services keys show $AKASH_KEY_NAME -a)"
-echo "wallet: $AKASH_ACCOUNT_ADDRESS"
+echo "Your Account Address: $AKASH_ACCOUNT_ADDRESS"
 export AKASH_NET="https://raw.githubusercontent.com/akash-network/net/main/mainnet"
 export AKASH_VERSION="$(curl -s https://api.github.com/repos/akash-network/provider/releases/latest | jq -r '.tag_name')"
 export AKASH_CHAIN_ID="$(curl -s "$AKASH_NET/chain-id.txt")"
 export AKASH_NODE="$(curl -s "$AKASH_NET/rpc-nodes.txt" | shuf -n 1)"
-echo "node: $AKASH_NODE, chain id: $AKASH_CHAIN_ID, backend: $AKASH_KEYRING_BACKEND"
+echo "Node URL: $AKASH_NODE"
+echo "Chain ID: $AKASH_CHAIN_ID"
 export AKASH_GAS=auto
 export AKASH_GAS_ADJUSTMENT=1.25
 export AKASH_GAS_PRICES=0.025uakt
