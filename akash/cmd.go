a24d1389461d7ff085f798082174d273e3d89677
will2z <will@fogmeta.com>
2023-09-27 17:39:29 +0800

    optimize: cmd support log sync

--- akash/cmd.go
+++ akash/cmd.go
@@ -1,20 +1,16 @@
 package akash
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func CreateAccount(name string) (err error) {
 	cmd := exec.Command("bash", "akash/create_account.sh", name)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println(string(out))
-	return
+	return DoCmd(cmd)
 }
 
 func Deploy(name, ymlPath string) (err error) {
@@ -23,11 +19,48 @@ func Deploy(name, ymlPath string) (err error) {
 		return
 	}
 	cmd := exec.Command("bash", "akash/deploy.sh", name, ymlPath)
-	out, err := cmd.CombinedOutput()
+	return DoCmd(cmd)
+}
+
+func syncLog(reader io.ReadCloser) {
+	cache := ""
+	buf := make([]byte, 1024)
+	for {
+		strNum, err := reader.Read(buf)
+		if strNum > 0 {
+			outputByte := buf[:strNum]
+			outputSlice := strings.Split(string(outputByte), "\n")
+			logText := strings.Join(outputSlice[:len(outputSlice)-1], "\n")
+			log.Printf("%s%s", cache, logText)
+			cache = outputSlice[len(outputSlice)-1]
+		}
+		if err != nil {
+			if err == io.EOF || strings.Contains(err.Error(), "file already closed") {
+				err = nil
+				if cache != "" {
+					log.Println(cache)
+				}
+			}
+		}
+	}
+}
+
+func DoCmd(cmd *exec.Cmd) (err error) {
+	cmdStdoutPipe, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	log.Println(string(out))
-	return
+	cmdStderrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if err = cmd.Start(); err != nil {
+		log.Println(err)
+		return
+	}
+	go syncLog(cmdStdoutPipe)
+	go syncLog(cmdStderrPipe)
+	return cmd.Wait()
 }
